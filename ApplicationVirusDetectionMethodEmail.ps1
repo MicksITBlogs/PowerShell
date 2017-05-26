423067571456364ada1933df722c96015aeb0cb7
Mick Pletcher <mick080645@gmail.com>
2017-05-26 13:01:24 -0500

    Add files via upload

---
+++ ApplicationVirusDetectionMethodEmail.ps1
@@ -0,0 +1,25 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+     Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.127
+     Created on:   	8/5/2016 11:11 AM
+	 Created by:   	Mick Pletcher
+	 Organization: 	 
+	 Filename:     	ApplicationVirusDetectionMethod.ps1
+	===========================================================================
+	.DESCRIPTION
+#>
+
+
+$LastInfection = get-winevent -filterhashtable @{ logname = 'system'; ID = 1116 } -maxevents 1 -ErrorAction SilentlyContinue
+$LastScan = Get-WinEvent -FilterHashtable @{ logname = 'system'; ProviderName = 'Microsoft Antimalware'; ID = 1001 } -MaxEvents 1
+If ($LastScan.TimeCreated -lt $LastInfection.TimeCreated) {
+	#No scan since last infection
+	Start-Sleep -Seconds 5
+	exit 0
+} else {
+	#No infection since last scan
+	Write-Host "No Infection"
+	Start-Sleep -Seconds 5
+	exit 0
+}
