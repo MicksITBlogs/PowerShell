5c861ce07afb1376a9375e233d8799b3cff3db2a
Mick Pletcher <mick080645@gmail.com>
2018-01-16 11:36:20 -0600

    Link Updated

---
+++ MouseMover.ps1
@@ -0,0 +1,32 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Rename Powershell Window
+$PowershellConsole = (Get-Host).UI.RawUI
+$PowershellConsole.WindowTitle = "Mouse Mover"
+
+Function BalloonTip{
+	[system.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null
+	$balloon = New-Object System.Windows.Forms.NotifyIcon
+	$path = Get-Process -id $pid | Select-Object -ExpandProperty Path
+	$icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+	$balloon.Icon = $icon
+	$balloon.BalloonTipIcon = 'Info'
+	$balloon.BalloonTipTitle = 'Secure Screen Stopper'
+	$balloon.BalloonTipText = 'Mouse will move in 15 seconds'
+	$balloon.Visible = $true
+	$balloon.ShowBalloonTip(30000)
+	Start-Sleep -Seconds 15
+}
+
+BalloonTip
+do{
+	$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+	[Windows.Forms.Cursor]::Position = "$($screen.Width*0.25),$($screen.Height*0.25)"
+	[Windows.Forms.Cursor]::Position = "$($screen.Width*0.75),$($screen.Height*0.25)"
+	[Windows.Forms.Cursor]::Position = "$($screen.Width*0.5),$($screen.Height*0.5)"
+	[Windows.Forms.Cursor]::Position = "$($screen.Width*0.25),$($screen.Height*0.75)"
+	[Windows.Forms.Cursor]::Position = "$($screen.Width*0.75),$($screen.Height*0.75)"
+	[System.Windows.Forms.SendKeys]::SendWait("{PGDN}")
+	Start-Sleep -Seconds 120
+	BalloonTip
+} Until (1 -eq 5)
---
+++ SecureScreenStopper.ps1
@@ -0,0 +1,37 @@
+﻿Clear-Host
+Add-Type -AssemblyName System.Windows.Forms
+
+# Rename Powershell Window
+$PowershellConsole = (Get-Host).UI.RawUI
+$PowershellConsole.WindowTitle = "Secure Screen Stopper"
+
+
+#Declare Variable
+$button = $null
+$Global:MainProcess
+$Global:RelativePath
+$Global:MMProcess
+
+Function GetRelativePath{
+	$Global:RelativePath=(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)+"\"
+	Write-Host $Global:RelativePath
+}
+
+Function KillProcesses{
+	$Running = Get-Process -Name conhost -ErrorAction SilentlyContinue
+	If($Running -ne $null){
+		Stop-Process -Name conhost -Force
+	}
+	$Running = Get-Process -Name powershell -ErrorAction SilentlyContinue
+	If($Running -ne $null){
+		Stop-Process -Name powershell -Force
+	}
+}
+
+GetRelativePath
+$a = "-WindowStyle Minimized -File"+[char]32+[char]34+$Global:RelativePath+"MouseMover.ps1"+[char]34
+Write-Host $a
+Start-Process powershell.exe $a
+$button = [system.windows.forms.messagebox]::show("Click OK to reinstate secure screen saver!")
+#Start-Sleep -Seconds 10
+KillProcesses
