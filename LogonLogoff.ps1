30321a1211eca312fc71bdfe4d0f4e58cdb101b1
Mick Pletcher <mick080645@gmail.com>
2018-10-26 16:04:46 -0500

    Added parsing for multiple IP addresses

--- LogonLogoff.ps1
+++ LogonLogoff.ps1
@@ -1,20 +1,17 @@
-﻿<#
+<#
 	.SYNOPSIS
-		Logon Detail
+		Logon Reporting
 	
 	.DESCRIPTION
-		This will report all logons to a centralized CSV file
+		This script will report the computername, username, IP address, and date/time to a central log file.
 	
-	.PARAMETER FileName
-		Name of CSV file
-	
-	.PARAMETER ReportPath
-		UNC path where report is generated
+	.PARAMETER LogFile
+		A description of the LogFile parameter.
 	
 	.NOTES
 		===========================================================================
 		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.142
-		Created on:   	10/11/2018 3:27 PM
+		Created on:   	10/22/2018 10:13 AM
 		Created by:   	Mick Pletcher
 		Filename:		LogonReport.ps1
 		===========================================================================
@@ -23,52 +20,29 @@
 param
 (
 	[ValidateNotNullOrEmpty()]
-	[string]$FileName = 'LogonReport.csv',
-	[ValidateNotNullOrEmpty()]
-	[string]$ReportPath = 'UNC Path'
+	[string]$LogFile = 'LogonReport.csv'
 )
 
-
-$Events = @()
-$Logons = (Get-WinEvent -FilterHashtable @{logname  = 'security'; ID = 4624}) | Where-Object {(($_.Message -notlike "*S-1-5-18*"))}
-foreach ($Logon in $Logons)
-{
+$Entries = @()
+$IPv4 = foreach ($ip in (ipconfig) -like '*IPv4*') {($ip -split ' : ')[-1]}
+$DT = Get-Date
+foreach ($IP in $IPv4) {
 	$object = New-Object -TypeName System.Management.Automation.PSObject
-	$AccountName = ((((($Logon.Message -split 'Account Domain:')[1]).Trim()) -split "Logon ID:")[0].Trim()) + '\' + ((((($Logon.Message -split 'Account Name:')[1].Trim())) -split 'Account Domain:')[0].Trim())
-	$SecurityID = (((($Logon.Message -split 'Security ID:')[1].Trim()) -split 'Account Name:')[0]).Trim()
-	$LogonType = (((($Logon.Message -split 'Logon Type:')[1].Trim()) -split 'Restricted Admin Mode:')[0]).Trim()
-	switch ($LogonType) {
-		'2' { $LogonType = 'Keyboard Logon'}
-		'3' { $LogonType = 'Remote File Share Logon' }
-		'4' { $LogonType = 'Scheduled Task Logon' }
-		'5' { $LogonType = 'Service Account Logon' }
-		'7' { $LogonType = 'Unlock Workstation' }
-		'8' { $LogonType = 'Network Clear Text Logon' }
-		'9' { $LogonType = 'Application RunAs Different Credentials' }
-		'10' { $LogonType = 'Remote Interactive Logon' }
-		'11' { $LogonType = 'Cached Credential Logon' }
-		default { $LogonType = 'Unknown Logon Type'}
-	}
-	If ($LogonType -ne 'Unknown Logon Type') {
-		$object | Add-Member -MemberType NoteProperty -Name ComputerName -Value ($Logon.MachineName).Split('.')[0]
-		$object | Add-Member -MemberType NoteProperty -Name UserName -Value $AccountName
-		$object | Add-Member -MemberType NoteProperty -Name TimeStamp -Value $Logon.TimeCreated
-		$object | Add-Member -MemberType NoteProperty -Name SecurityID -Value $SecurityID
-		$object | Add-Member -MemberType NoteProperty -Name LogonType -Value $LogonType
-		$Events += $object
-	}
+	$object | Add-Member -MemberType NoteProperty -Name ComputerName -Value $env:COMPUTERNAME
+	$object | Add-Member -MemberType NoteProperty -Name UserName -Value $env:USERNAME
+	$object | Add-Member -MemberType NoteProperty -Name IPAddress -Value $IP
+	$object | Add-Member -MemberType NoteProperty -Name DateTime -Value (Get-Date)
+	$object
+	$Entries += $object
 }
-$Events
-If ($ReportPath.Substring($ReportPath.Length - 1) -ne '\') {
-	$ReportPath += '\'
+foreach ($Entry in $Entries) {
+	Do {
+		Try {
+			Export-Csv -InputObject $Entry -Path $LogFile -Encoding UTF8 -NoTypeInformation -NoClobber -Append
+			$Success = $true
+		} Catch {
+			$Success = $false
+			Start-Sleep -Seconds 1
+		}
+	} while ($Success -eq $false)
 }
-$File = $ReportPath + $FileName
-Do {
-	Try {
-		$Events | ConvertTo-Csv -NoTypeInformation | select -Skip 1 | Out-File -FilePath $File -Encoding UTF8 -Append
-		$Success = $true
-	} Catch {
-		Start-Sleep -Seconds 2
-		$Success = $false
-	}
-} while ($Success -eq $false)
